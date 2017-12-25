b51c4954f6246032250c1312e0f84c9ee8b0ed45
Caio Salvador <caiosalvador96@gmail.com>
2017-12-24 23:54:16 -0200

    Update pc-update alias

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -52,7 +52,7 @@ alias ...='cd ../../'
 alias ....='cd ../../../'
 alias .....='cd ../../../../'
 alias ......='cd ../../../../../'
-alias pc-update="sudo apt-get update && sudo apt-get -y upgrade && sudo apt-get -y autoremove && sudo apt-get -y autoclean"
+alias pc-update="sudo apt update && sudo apt -y upgrade && sudo apt-get -y autoremove && sudo apt -y autoclean"
 # Add an "alert" alias for long running commands.  Use like so: "sleep 10; alert"
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 alias network-restart="service network-manager restart"
