e6b0623d61d01db7e875c672c7233e10a4271c9f
Caio Salvador Rohwedder <caiosalvador96@gmail.com>
2023-06-10 21:14:26 -0600

    Separate previous nvim lua config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,8 @@
 --Config for neovim
 
-require('settings')
-require('mappings')
-require('auto-cmd')
-require('plugins')
-require('plugins-config')
-require('lsp-completion')
+require('caio-conf.settings')
+require('caio-conf.mappings')
+require('caio-conf.auto-cmd')
+require('caio-conf.plugins')
+require('caio-conf.plugins-config')
+require('caio-conf.lsp-completion')
--- .config/nvim/lua/caio-conf/auto-cmd.lua
+++ .config/nvim/lua/caio-conf/auto-cmd.lua
@@ -1,13 +1,3 @@
--- Highlight on yank
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-  group = highlight_group,
-  pattern = '*',
-})
-
 -- Triger autoread when files changes on disk
 vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
     command = "if mode() != 'c' | checktime | endif",
