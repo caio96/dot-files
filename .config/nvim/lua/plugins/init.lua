b7509b48e0f2fcc8e704abaff0f9f181ed0c6aed
Caio Salvador Rohwedder <caiosalvador96@gmail.com>
2024-04-05 14:34:43 -0600

    Update nvim config: add CopilotChat

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -485,4 +485,24 @@ return {
       end,
     },
   },
+
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    cmd = {
+      "CopilotChat",
+      "CopilotChatOpen",
+      "CopilotChatClose",
+      "CopilotChatToggle",
+      "CopilotChatReset",
+      "CopilotChatSave",
+      "CopilotChatLoad",
+      "CopilotChatDebugInfo",
+    },
+    branch = "canary",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+    },
+    opts = {},
+  },
 }
