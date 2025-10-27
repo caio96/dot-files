79f030a7b446aee1f63ec40953f144a700bc561a
Caio Salvador Rohwedder <Caio.SalvadorRohwedder@amd.com>
2025-10-26 20:51:10 -0600

    Use builtin copilot in neovim

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -45,6 +45,7 @@ return {
         "bash-language-server",
         "cmake-language-server",
         "clangd",
+        "copilot-language-server",
         "dockerfile-language-server",
         "lua-language-server",
         "python-lsp-server",
@@ -537,27 +538,6 @@ return {
     "numToStr/Comment.nvim",
     opts = {},
     keys = { "gcc", "gbc", { "gc", "gb", mode = "v" } },
-  },
-
-  {
-    "github/copilot.vim",
-    cmd = "Copilot",
-    event = "BufWinEnter",
-    version = "v1.53.0",
-    init = function()
-      vim.g.copilot_no_maps = true
-    end,
-    config = function()
-      -- Block the normal Copilot suggestions
-      vim.api.nvim_create_augroup("github_copilot", { clear = true })
-      vim.api.nvim_create_autocmd({ "FileType", "BufUnload" }, {
-        group = "github_copilot",
-        callback = function(args)
-          vim.fn["copilot#On" .. args.event]()
-        end,
-      })
-      vim.fn["copilot#OnFileType"]()
-    end,
   },
 
   {
